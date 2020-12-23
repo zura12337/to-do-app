5a2a6dccf8f609dd07289731d402111f6c5e9b1b
zura12337 <zura.kurta@gmail.com>
2020-12-24 00:14:34 +0400

    ✨ Implement trash functionally.

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,28 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { getTasks, reorderTasks } from "../services/tasks";
 // @ts-ignore
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import Block from "./Block";
+// @ts-ignore
+import { CSSTransition } from "react-transition-group";
 
 export default function List() {
   const [tasks, setTasks] = useState([]);
+  const [showPopover, setShowPopover] = useState(false);
 
   useEffect(() => {
     const newTasks: [] = getTasks();
     setTasks(newTasks);
-  }, [tasks]);
+  }, []);
 
   const handleDragEnd = (result: any) => {
+    console.log(result.destination);
     if (!result.destination) return;
     const newTasks = [...tasks];
-
-    if (result.source.droppableId !== result.destination.droppableId) {
-      let item: { type: string; name: string } = newTasks[result.source.index];
-      item.type = result.destination.droppableId;
+    if (result.destination.droppableId === "trash") {
+      newTasks.splice(result.source.index, 1);
+    } else {
+      if (result.source.droppableId !== result.destination.droppableId) {
+        let item: { type: string; name: string } =
+          newTasks[result.source.index];
+        item.type = result.destination.droppableId;
+      }
+      const [reorderedTasks] = newTasks.splice(result.source.index, 1);
+      newTasks.splice(result.destination.index, 0, reorderedTasks);
     }
-    const [reorderedTasks] = newTasks.splice(result.source.index, 1);
-    newTasks.splice(result.destination.index, 0, reorderedTasks);
-
     reorderTasks(newTasks);
     setTasks(newTasks);
   };
@@ -33,6 +40,40 @@ export default function List() {
         <Block tasks={tasks} listName="to-do" listTitle={"To Do"} />
         <Block tasks={tasks} listName="in-progress" listTitle={"In Progress"} />
         <Block tasks={tasks} listName="done" listTitle={"Done"} />
+        <Droppable droppableId="trash">
+          {(provided: any, snapshot: any) => (
+            <div
+              className="trash"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              <div className="trash-inner">
+                <CSSTransition
+                  in={showPopover}
+                  timeout={300}
+                  classNames="trash"
+                  unmountOnExit
+                >
+                  <div className="trash-popover">
+                    <p>Drag Any item here to remove them. 🗑</p>
+                  </div>
+                </CSSTransition>
+                {/* {showPopover && (
+                  <div className="trash-popover">
+                    <p>Drag Any item here to remove them. 🗑</p>
+                    <button
+                      className="popover-close"
+                      onClick={() => setShowPopover(false)}
+                    >
+                      X
+                    </button>
+                  </div>
+                )} */}
+                <h3 onClick={() => setShowPopover(!showPopover)}>🗑</h3>
+              </div>
+            </div>
+          )}
+        </Droppable>
       </DragDropContext>
     </div>
   );
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -6,26 +6,31 @@ import Item from "./Item";
 export default function ListItem({ name, index, type }: Item) {
   return (
     <Draggable draggableId={`${name}-${index}`} index={index} key={index}>
-      {(provided: any, snapshot: any) => (
-        <div
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-          style={{
-            backgroundColor: snapshot.isDragging
-              ? "#ebebeb"
-              : type === "done"
-              ? "#888888"
-              : "#ffffff",
-            color: type === "done" && !snapshot.isDragging && "white",
-            animation: ".2s all",
-            ...provided.draggableProps.style,
-          }}
-          className="list-item"
-        >
-          <span className="item-title">{name}</span>
-        </div>
-      )}
+      {(provided: any, snapshot: any) => {
+        if (snapshot.draggingOver === "trash") {
+          console.log(snapshot);
+        }
+        return (
+          <div
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            style={{
+              backgroundColor: snapshot.isDragging
+                ? "#ebebeb"
+                : type === "done"
+                ? "#888888"
+                : "#ffffff",
+              color: type === "done" && !snapshot.isDragging && "white",
+              animation: ".2s all",
+              ...provided.draggableProps.style,
+            }}
+            className="list-item"
+          >
+            <span className="item-title">{name}</span>
+          </div>
+        );
+      }}
     </Draggable>
   );
 }
